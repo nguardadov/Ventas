4f5f70293e5d7be91adf8087dacb3480a0b093d0
nguardadov <nguardadov@gmail.com>
2018-04-28 18:05:31 -0600

    Ventanas de login

---
+++ app/src/main/java/com/guardado/comunicacionf/OpcionesActivity.java
@@ -0,0 +1,12 @@
+package com.guardado.comunicacionf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpcionesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opciones);
+    }
+}
